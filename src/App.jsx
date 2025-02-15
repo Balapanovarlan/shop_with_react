f45ac3b473a14a78cf27599a1cde825cf2ef6dc5
arlan balapanov <mrx11y@gmail.com>
2025-02-15 20:23:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+import { BrowserRouter, Routes,  Route } from "react-router"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Button>Check</Button>
+    </BrowserRouter>
+  )
+}
+
+export default App
